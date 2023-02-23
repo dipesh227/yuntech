b4e136709c831e7c581917c674dee02335c8612d
dipesh227 <dipeshjoshi227@gmail.com>
2023-02-23 16:10:01 +0530

    css js fix + navbar

---
+++ resources/views/components/footerjslink.blade.php
@@ -0,0 +1,73 @@
+<script src="../../vendor/jquery/dist/jquery.min.js"></script>
+<script src="../../vendor/popper.js/dist/umd/popper.min.js"></script>
+<script src="../../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="../../vendor/headroom.js/dist/headroom.min.js"></script>
+<script src="../../vendor/onscreen/dist/on-screen.umd.min.js"></script>
+<script src="../../vendor/nouislider/distribute/nouislider.min.js"></script>
+<script src="../../vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="../../vendor/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="../../vendor/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="../../vendor/jarallax/dist/jarallax.min.js"></script>
+<script src="../../vendor/jquery.counterup/jquery.counterup.min.js"></script>
+<script src="../../vendor/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="../../vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+<script src="../../vendor/prismjs/prism.js"></script>
+<script async defer="defer" src="../../../../buttons.github.io/buttons.js"></script>
+<script src="../../assets/js/neumorphism.js"></script>
+<script>
+    $toggleTransitions = $("#toggleTransitions");
+    $buttonOverlay = $(".btn-loading-overlay");
+
+    $toggleTransitions.on("change", function() {
+        if ($(this).is(":checked")) {
+            $buttonOverlay.addClass("btn-loading");
+            $buttonOverlay.attr("disabled", true);
+        } else {
+            $buttonOverlay.removeClass("btn-loading");
+            $buttonOverlay.attr("disabled", false);
+        }
+    });
+</script>
+<script>
+    (function() {
+        var js =
+            "window['__CF$cv$params']={r:'79d5430478eabb71',m:'L_0IkLp7NuWS5ORD8BeK31BqBdJhZTgrPQENyJo2rdY-1677043113-0-AS+4SOBwkdPSL96M5+yEnhQLJi70UYfm+6dcwMytDZdFT8p/WJ8GhX6qZSBuXpnbl+ZhuabgOX+XjJdhAhQqlVK5gjaaPmbYqthgfCg8lIB8DPFZ3OPdmWx6sIsk+ar/5Sx9yZWNt8PysxbErd74CSrxch/0S9BJxgcUYlHUzbAB',s:[0xd7b6e29783,0xe25547ac18],u:'/cdn-cgi/challenge-platform/h/g'};var now=Date.now()/1000,offset=14400,ts=''+(Math.floor(now)-Math.floor(now%offset)),_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='../../../cdn-cgi/challenge-platform/h/g/scripts/alpha/invisible5615.js?ts='+ts,document.getElementsByTagName('head')[0].appendChild(_cpo);";
+        var _0xh = document.createElement("iframe");
+        _0xh.height = 1;
+        _0xh.width = 1;
+        _0xh.style.position = "absolute";
+        _0xh.style.top = 0;
+        _0xh.style.left = 0;
+        _0xh.style.border = "none";
+        _0xh.style.visibility = "hidden";
+        document.body.appendChild(_0xh);
+
+        function handler() {
+            var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;
+            if (_0xi) {
+                var _0xj = _0xi.createElement("script");
+                _0xj.nonce = "";
+                _0xj.innerHTML = js;
+                _0xi.getElementsByTagName("head")[0].appendChild(_0xj);
+            }
+        }
+        if (document.readyState !== "loading") {
+            handler();
+        } else if (window.addEventListener) {
+            document.addEventListener("DOMContentLoaded", handler);
+        } else {
+            var prev = document.onreadystatechange || function() {};
+            document.onreadystatechange = function(e) {
+                prev(e);
+                if (document.readyState !== "loading") {
+                    document.onreadystatechange = prev;
+                    handler();
+                }
+            };
+        }
+    })();
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
+    integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA=="
+    data-cf-beacon='{"rayId":"79d5430478eabb71","version":"2023.2.0","r":1,"token":"3a2c60bab7654724a0f7e5946db4ea5a","si":100}'
+    crossorigin="anonymous"></script>
---
+++ resources/views/components/headercsslink.blade.php
@@ -0,0 +1,69 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Neumorph Pro Components - All</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+    <meta name="title" content="Neumorph Pro Components - All" />
+    <meta name="author" content="Themesberg" />
+    <link rel="canonical" href="https://themesberg.com/product/ui-kit/neumorphism-ui-kit-bootstrap-pro" />
+    <meta name="keywords"
+        content="neumorphism, neumorphism ui, neomorphism, neomorphism ui, neomorphism css, neumorphism css, neumorph, neumorphic, design system, login, form, table, tables, card, cards, navbar, modal, icons, icons, map, chat, carousel, menu, datepicker, gallery, slider, date, social, dropdown, search, tab, nav, footer, date picker, forms, tabs, time, button, select, input, timeline, cart, about us, account, log in, blog, profile, portfolio, landing page, ecommerce, shop, landing, register, app, contact, one page, sign up, signup, store, bootstrap 4, bootstrap4, dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, themesberg, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit" />
+    <meta name="description"
+        content="Start developing neumorphic web applications and pages using Neumorphism UI PRO. It features over 1000 individual components and 13 example pages." />
+    <meta itemprop="name" content="Neumorphism UI PRO by Themesberg" />
+    <meta itemprop="description"
+        content="Start developing neumorphic web applications and pages using Neumorphism UI PRO. It features over 1000 individual components and 13 example pages." />
+    <meta itemprop="image"
+        content="../../../../themesberg.s3.us-east-2.amazonaws.com/public/products/neumorphism-ui-pro/neumorphism-thumbnail.jpg" />
+    <meta name="twitter:card" content="product" />
+    <meta name="twitter:site" content="@themesberg" />
+    <meta name="twitter:title" content="Neumorphism UI PRO by Themesberg" />
+    <meta name="twitter:description"
+        content="Start developing neumorphic web applications and pages using Neumorphism UI PRO. It features over 1000 individual components and 13 example pages." />
+    <meta name="twitter:creator" content="@themesberg" />
+    <meta name="twitter:image"
+        content="../../../../themesberg.s3.us-east-2.amazonaws.com/public/products/neumorphism-ui-pro/neumorphism-thumbnail.jpg" />
+    <meta property="fb:app_id" content="214738555737136" />
+    <meta property="og:title" content="Neumorphism UI PRO by Themesberg" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="../../index.html" />
+    <meta property="og:image"
+        content="../../../../themesberg.s3.us-east-2.amazonaws.com/public/products/neumorphism-ui-pro/neumorphism-thumbnail.jpg" />
+    <meta property="og:description"
+        content="Start developing neumorphic web applications and pages using Neumorphism UI PRO. It features over 1000 individual components and 13 example pages." />
+    <meta property="og:site_name" content="Themesberg" />
+    <link rel="apple-touch-icon" sizes="120x120" href="../../assets/img/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/img/favicon/favicon-16x16.png" />
+    <link rel="manifest" href="../../assets/img/favicon/site.webmanifest" />
+    <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#ffffff" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
+    <link type="text/css" href="../../vendor/%40fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
+    <link type="text/css" href="../../css/neumorphism.css" rel="stylesheet" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141734189-6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "UA-141734189-6");
+    </script>
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                "gtm.start": new Date().getTime(),
+                event: "gtm.js"
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src =
+                "../../../../www.googletagmanager.com/gtm5445.html?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, "script", "dataLayer", "GTM-THQTXJ7");
+    </script>
+</head>
